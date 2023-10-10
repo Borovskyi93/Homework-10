345a781ad089190a80654d724f932c84e435f45c
Borovskyi93 <godofgrime@gmail.com>
2023-10-10 13:57:53 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 const lengthOfArrayFromUser = +prompt('Введіть довжину масиву:');
-const arrayNumbers = [];
 
 if (lengthOfArrayFromUser) {
-    for (let countOfIndex = 0; countOfIndex < lengthOfArrayFromUser; countOfIndex++) {
-        const elementsOfArrayFromUser = +prompt('Введіть елементи масива:');
-        arrayNumbers.push(elementsOfArrayFromUser);
-    }
+    const elementsOfArrayFromUser = prompt('Введіть елементи масива через пробіл:');
+    const arrayNumbers = elementsOfArrayFromUser.split(' ');
     console.log('Масив до сортування:', arrayNumbers);
-    const sortedArrayNumbers = arrayNumbers.toSorted((a, b) => a - b );
-    console.log('Масив після сортування:', sortedArrayNumbers);
-    sortedArrayNumbers.splice(1, 3);
-    console.log('Масив після видалення елементів:', sortedArrayNumbers);
+
+    if (lengthOfArrayFromUser === arrayNumbers.length) {
+        for (let indexArray = 0; indexArray < arrayNumbers.length; indexArray++) {
+            if (isNaN(arrayNumbers[indexArray])) {
+                console.log('Неправильні дані');
+            } 
+        }
+
+        const sortedArrayNumbers = arrayNumbers.toSorted((a, b) => a - b );
+        console.log('Масив після сортування:', sortedArrayNumbers);
+        sortedArrayNumbers.splice(1, 3);
+        console.log('Масив після видалення елементів:', sortedArrayNumbers);
+
+    } else if (lengthOfArrayFromUser < arrayNumbers.length || lengthOfArrayFromUser > arrayNumbers.length) {
+        console.log('Невідповідність довжини масива та кількості елементів!');
+    } 
 } else {
     console.log('Щось пішло не так!');
 }
+
